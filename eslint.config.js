72e4147dc4951fb142c53a1a8bee148cccac1cb7
Jefferson Cruz <cruzjeff225@gmail.com>
2026-09-18 21:05:52 -0600

    chore: configure eslint, prettier and editorconfig

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  prettier,
+)

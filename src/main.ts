064ca58b19dd4e66451cd4f61a866d0e32e42612
Jefferson Cruz <cruzjeff225@gmail.com>
2026-09-18 21:16:25 -0600

    feat: add design tokens and light/dark theme styles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import '@fontsource-variable/inter'
+import '@fontsource-variable/jetbrains-mono'
 import { createApp } from 'vue'
 import App from './App.vue'
+import './styles/index.css'
 
 createApp(App).mount('#app')
